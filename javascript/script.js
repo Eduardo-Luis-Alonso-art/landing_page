c66c12ce6fd9b50720c458f33588a222653f9e4f
Eduardo Luis Alonso <eduardoluisalonsi@gmail.com>
2025-08-13 16:17:29 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -18,5 +18,47 @@ $(document).ready(function(){
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
         }
+
+        sections.each(function(i){
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop+ section.outerHeight();
+
+            if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+                activeSectionIndex = i
+                return false;
+            }
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
+
+    ScrollReveal().reveal('#cta',{
+        origin: 'left',
+        duration: 2000,
+        distance: '20%',
+        reset: true
+    });
+
+    ScrollReveal().reveal('.dish',{
+        origin: 'left',
+        duration: 2000,
+        distance: '20%',
+        reset: true
+    });
+
+    ScrollReveal().reveal('#testimonial_chef',{
+        origin: 'left',
+        duration: 1000,
+        distance: '20%',
+        reset: true
+    });
+
+    ScrollReveal().reveal('.feedback',{
+        origin: 'right',
+        duration: 2000,
+        distance: '20%',
+        reset: true
     });
 });
